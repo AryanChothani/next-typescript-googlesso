c57f38fad5bce03dc5675ffbee6c589a60f1db53
Arya4514 <aryan_chothani@favarr.com>
2023-10-25 23:50:14 +0530

    initial commit

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -2,6 +2,7 @@
 import { useSession, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import style from '../../styles/login.module.scss'
+import Image from "next/image";
 
 
 export default function Login() {
@@ -16,7 +17,13 @@ export default function Login() {
             <>
                 <div className={style.googleBtn}>
                     <div className={style.googleIconWrapper}>
-                        <img className={style.googleIcon} src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
+                            <Image
+                                className={style.googleIcon}
+                                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                                width={100}
+                                height={100}
+                                alt="Picture of the google"
+                            />
                     </div>
                     <p className={style.btnText} onClick={() => signIn()}><b >Sign in with google</b></p>
                 </div>
